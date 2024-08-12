7273cf3e115f3d7017f3c2fb344abe6f0ac5b6b9
honey32 <honey32@bearwapps.com>
2024-08-12 10:50:11 +0900

    fix: トップページ等のいらない記述を整理

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        // Windows で Yu Gothic UI になるのが余計なので、デフォルトに戻す。
+        sans: ["sans-serif"],
       },
     },
   },
